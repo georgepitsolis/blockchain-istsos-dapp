82e3eb31d39a9f188491393f6fe2c5c0d2a2b465
georgepitsolis <georgepitsolis@gmail.com>
2022-03-29 13:43:02 +0300

    submit button

--- firstdapp/routes/add.js
+++ firstdapp/routes/add.js
@@ -1,18 +1,10 @@
+require('dotenv').config();
 var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res) {
     res.render('pages/add', {
-        title: 'Add files page',
-        selected_files: [
-            {
-            station_name:'ASIGONIA',
-            file_name:'ASIGONIA_20220328193.txt' 
-            }, {
-            station_name:'ASIGONIA',
-            file_name:'ASIGONIA_20220328193.txt' 
-            }            
-        ]        
+        title: 'Add files page'
     });
 });
 
